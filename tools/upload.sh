505359ff8e3ab8e67fef1389e6dc6dcec56a2949
hitimr <hitimr@gmail.com>
2023-09-27 22:21:08 +0200

    improved upload scrip

--- tools/upload.sh
+++ tools/upload.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-sudo chown hiti /dev/ttyUSB0
+sudo chown "$USER" /dev/ttyUSB0
 
 USB_PORT="/dev/ttyUSB0"  # Replace with your USB port
 BAUD_RATE="115200"       # Set to your desired baud rate. 115200 is commonly used for ESP32.
 
-# Loop through each .py file in the src directory
-for file in src/*.py; do
+# Check if an argument is passed, and set the source directory accordingly
+SRC_DIR="src"
+if [ "$#" -eq 1 ]; then
+    SRC_DIR="$1"
+fi
+
+# Loop through each .py file in the source directory
+for file in "$SRC_DIR"/*.py; do
     # Extract the filename without the path
     filename=$(basename "$file")
     # Upload the file
